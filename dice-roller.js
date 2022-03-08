e2d852e039aabb48fdce7f6e473c8c6097c1ddf9
Lisa Meyer <lisamermey@gmail.com>
2022-03-08 16:20:54 -0500

    working function

--- dice-roller.js
+++ dice-roller.js
@@ -4,11 +4,26 @@
 * @result {String} "Rolled n dice: n"
 */
 
-// retrieve command line argument and store value
-const arg = process.argv;
-const n = arg.slice(2);
 
 // generate a random number n times, helper function
 // store as an array and return random number n times
+const randomNum = n => {
+  const arr = [];
+  
+  while (arr.length < n){
+    let random = Math.floor(Math.random() * 6 + 1);
+    arr.push(random);
+  }
+  return arr.join('').split('').join(', ');
+};
+
+// retrieve command line argument and store value
+const arg = process.argv;
+let n = arg.slice(2);
+
+console.log(`Rolled ${n} dice: ${randomNum(n)}`);
+
+
+
 
 // 
