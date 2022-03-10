4770632b3b24494238a12285f0d1aeeb9e4ef265
Lisa Meyer <lisamermey@gmail.com>
2022-03-10 09:42:19 -0500

    set condition to check if data point exists in results object, established a getTotalSales function to be used main function

--- objects/sales_tax/sales.js
+++ objects/sales_tax/sales.js
@@ -10,9 +10,25 @@ const calculateSalesTax = (salesData, taxRates) => {
   // iterate over sales data array of objects
   for (const data of salesData) {
     // check if data does not exist in results object
-
-    // add company name to results object
+    if (!results.hasOwnProperty('name')) {
+      // add company information to results object
+      results['name'] = data['name'];
+      results['totalSales'] = 0;
+      results['totalTaxes'] = 0;
+    }
     // create totalSales accumulator key to add to results object; could be its own function
     // create totalTaxes accumulator key to add to results object; could be its own function
   }
 };
+
+// Function to get total company sales
+const getTotalSales = (salesData, results) => {
+
+  // iterate over salesData array of objects and target value in object
+  for (const sale of salesData) {
+    
+  }
+
+};
+
+// Function to 
