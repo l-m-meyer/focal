f5087a2efa7e8f70346f4c7a433aeee9cc032364
Lisa Meyer <lisamermey@gmail.com>
2022-03-10 14:01:41 -0500

    refactored to use .forEach() for findWaldo()

--- functions/callbacks-intro/findWaldo.js
+++ functions/callbacks-intro/findWaldo.js
@@ -1,11 +1,11 @@
 // The second argument/parameter is expected to be a (callback) function
 const findWaldo = function(names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let name = names[i];
+  names.forEach((ele, index) => {
+    let name = ele;
     if (name === "Waldo") {
-      found(i);   // execute callback
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
 const actionWhenFound = function(index) {
