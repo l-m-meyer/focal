e33fb9e54caf70d50e12253cf2b4a129177c3a59
Lisa Meyer <lisamermey@gmail.com>
2022-03-10 13:28:39 -0500

    modified the actionWhenFound function to let it receive and use the index, modified findWaldo function so it passes the index to the callback

--- functions/callbacks-intro/findWaldo.js
+++ functions/callbacks-intro/findWaldo.js
@@ -3,13 +3,13 @@ const findWaldo = function(names, found) {
   for (let i = 0; i < names.length; i++) {
     let name = names[i];
     if (name === "Waldo") {
-      found();   // execute callback
+      found(i);   // execute callback
     }
   }
 }
 
-const actionWhenFound = function() {
-  console.log("Found him!");
+const actionWhenFound = function(index) {
+  console.log(`Found Waldo at index ${index}!`);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
