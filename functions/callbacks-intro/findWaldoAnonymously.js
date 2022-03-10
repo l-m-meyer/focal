85c7d898abc8f59ebdae6a2de3c6cba903e39648
Lisa Meyer <lisamermey@gmail.com>
2022-03-10 14:09:58 -0500

    initial commit

---
+++ functions/callbacks-intro/findWaldoAnonymously.js
@@ -0,0 +1,15 @@
+// The second argument/parameter is expected to be a (callback) function
+const findWaldo = function(names, found) {
+  names.forEach((ele, index) => {
+    let name = ele;
+    if (name === "Waldo") {
+      found(index);   // execute callback
+    }
+  });
+}
+
+const actionWhenFound = function(index) {
+  console.log(`Found Waldo at index ${index}!`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
