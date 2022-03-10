0fd4588132c5351cc058fd00cd36bde029a53c54
Lisa Meyer <lisamermey@gmail.com>
2022-03-09 22:33:24 -0500

    initial commit, added pseudocode

---
+++ objects/sales_tax/sales.js
@@ -0,0 +1,18 @@
+/*
+* Function calculates the total sales and total tax, grouped by company.
+* @param {Array} salesData -- an Array of Objects
+* @param {Object} taxRates 
+* @return {Ojbect} results
+*/
+const calculateSalesTax = (salesData, taxRates) => {
+  const results = {};
+
+  // iterate over sales data array of objects
+  for (const data of salesData) {
+    // check if data does not exist in results object
+
+    // add company name to results object
+    // create totalSales accumulator key to add to results object; could be its own function
+    // create totalTaxes accumulator key to add to results object; could be its own function
+  }
+};
